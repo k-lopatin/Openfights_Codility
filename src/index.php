ba7a69079f6ad821decde10bc2ba006c10766d01
Konstantin Lopatin <k.lopatin@cognitive.ru>
2017-09-16 15:59:38 +0300

    Simple silex application

--- src/index.php
+++ src/index.php
@@ -1,2 +1,17 @@
 <?php
-phpinfo();
+
+require __DIR__ . '/vendor/autoload.php';
+
+function getCurrentBalance() {
+  $cardInfo = [
+    'CardId' => 42
+  ];
+  $uri = 'https://api.open.ru/MyCards/1.0.0/MyCardsInfo/balance';
+  $body = json_encode($cardInfo);
+  return \Httpful\Request::post($uri)
+                              ->body($body)
+                              ->sendsJson()
+                              ->send();
+}
+
+print_r(getCurrentBalance());
